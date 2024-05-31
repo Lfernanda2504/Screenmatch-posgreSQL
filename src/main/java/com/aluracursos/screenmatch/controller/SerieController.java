9b9d5bde4a679f9da328e46047f25e7a7e55b677
lfgarciaoc <luizafernandagarcia@gmail.com>
2024-05-31 12:50:22 -0500

    feat: creando controller y datos que se muestran en el servidor

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,15 +1,34 @@
 package com.aluracursos.screenmatch.controller;
 
 
+import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.model.Serie;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class SerieController {
+    // inyección de dependencias
+    @Autowired
+    private SerieRepository repository;
+
+
     //para obtener datos de una ruta en especifico
     //endpoint
     @GetMapping("/series")
-    public  String mostrarMensaje(){
-        return "Mensaje aplicación web";
+    public List<SerieDTO> obtenerSeries(){
+        //return series
+        return repository.findAll().stream()
+                //conversion de tipo serie a tipo serieDTO
+                .map(s-> new SerieDTO(s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(), s.getPoster(),
+                        s.getGenero(), s.getActores(), s.getSinopsis()))
+                .collect(Collectors.toList());
     }
+
+
 }
